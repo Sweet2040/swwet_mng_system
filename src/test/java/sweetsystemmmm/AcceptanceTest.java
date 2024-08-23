10254804cdcea87a3cec1819f006c126f5833b52
shahddrawajbeh <s12114152@stu.najah.edu>
2024-08-23 10:16:23 +0300

    Update AcceptanceTest.java

--- src/test/java/sweetsystemmmm/AcceptanceTest.java
+++ src/test/java/sweetsystemmmm/AcceptanceTest.java
@@ -11,7 +11,7 @@ import io.cucumber.junit.CucumberOptions.SnippetType;
 
 @RunWith(Cucumber.class)
 
-@CucumberOptions(features="MyFeatures" ,
+@CucumberOptions(features="src/test/resources/MyFeatures" ,
 plugin= {"summary", "html:target/cucumber/report.html"},
 monochrome=true,
 snippets=SnippetType.CAMELCASE,
