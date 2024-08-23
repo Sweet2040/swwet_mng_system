5adb7eba7e481362dece1eef0787b22a66ab78ec
shahddrawajbeh <s12114152@stu.najah.edu>
2024-08-23 20:17:18 +0300

    Update AcceptanceTest.java

--- src/test/java/sweetsystemmmm/AcceptanceTest.java
+++ src/test/java/sweetsystemmmm/AcceptanceTest.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 
 
 
-
+import io.cucumber.*;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.CucumberOptions.SnippetType;
