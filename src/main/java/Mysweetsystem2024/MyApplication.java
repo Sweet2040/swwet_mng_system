1ad52d904724708f70b4dc0be8ecbbc558de7b34
shahddrawajbeh <s12114152@stu.najah.edu>
2024-08-24 16:24:33 +0300

    Update MyApplication.java

--- src/main/java/Mysweetsystem2024/MyApplication.java
+++ src/main/java/Mysweetsystem2024/MyApplication.java
@@ -1,36 +1,18 @@
 package Mysweetsystem2024;
 
-
-
-
 import java.util.HashMap;
-
 import java.util.Map;
-
-
-
-
 
 public class MyApplication {
     private Map<String, User> users;
-   
-
-    
-    private LoginManager loginManager;
-    
-    
-    
 
     public MyApplication() {
         users = new HashMap<>();
-        loginManager = new LoginManager(users);
-       
-      
     }
+
     public void signUpUser(String username, String password, String email, String country, UserRole role) {
         if (!userExists(username)) {
             users.put(username, new User(username, password, email, country, role));
-            
         }
     }
 
@@ -38,22 +20,16 @@ public class MyApplication {
         return users.containsKey(username);
     }
 
-    
- 
-
     public User getUser(String username) {
         return users.get(username);
     }
+
     public boolean addUser(User user) {
         if (userExists(user.getUsername())) {
             return false;
         }
         users.put(user.getUsername(), user);
         return true;
-    
-      
-        
-        
     }
-    
 }
+
