a417f731293c9300937176867fbbd8309b6277b5
shahddrawajbeh <s12114152@stu.najah.edu>
2024-08-25 00:26:44 +0300

    Update Monitoringsteps.java

--- src/test/java/sweetsystemmmm/Monitoringsteps.java
+++ src/test/java/sweetsystemmmm/Monitoringsteps.java
@@ -5,7 +5,7 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import static org.junit.Assert.*;
 
-import mysweetsystem.Test;
+import Mysweetsystem2024.Test;
 
 public class Monitoringsteps {
 
