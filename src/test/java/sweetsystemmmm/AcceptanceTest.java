496d9c39d68d9a69feb411fb9b732ebe7be4ea51
shahddrawajbeh <s12114152@stu.najah.edu>
2024-08-24 12:15:03 +0300

    Update AcceptanceTest.java

--- src/test/java/sweetsystemmmm/AcceptanceTest.java
+++ src/test/java/sweetsystemmmm/AcceptanceTest.java
@@ -11,7 +11,7 @@ import io.cucumber.junit.*;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(
-  features= "C:/Users/acer/eclipse-workspace/sweetsystem/MyFeatures" ,
+  features= "C:\Users\acer\git\swwet_mng_system11\Myfeatures" ,
 plugin= {"summary", "html:target/cucumber/report.html"},
 monochrome=true,
 snippets=SnippetType.CAMELCASE,
