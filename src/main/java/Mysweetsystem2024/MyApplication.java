5fdf5aa6f5b9ac0415452e912f1e8f8e48df952f
shahddrawajbeh <s12114152@stu.najah.edu>
2024-08-24 14:58:59 +0300

    Update MyApplication.java

--- src/main/java/Mysweetsystem2024/MyApplication.java
+++ src/main/java/Mysweetsystem2024/MyApplication.java
@@ -11,7 +11,6 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
