3284e3b14217e6aa54b2a38c64433dd49ace50c9
shahddrawajbeh <s12114152@stu.najah.edu>
2024-08-25 20:18:03 +0300

    Update LoginManager.java

--- src/main/java/Mysweetsystem2024/LoginManager.java
+++ src/main/java/Mysweetsystem2024/LoginManager.java
@@ -31,17 +31,6 @@ public class LoginManager {
 
 
 
-
-
-
-
-    
-    
-    private boolean validateLogin(String username, String password) {
-        User user = users.get(username);
-        return user != null && user.getPassword().equals(password);
-    }
-
    
 
 
