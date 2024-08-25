35903110d45e5187af5c70188fd6b8655758aa8f
shahddrawajbeh <s12114152@stu.najah.edu>
2024-08-25 17:13:52 +0300

    Update LoginManager.java

--- src/main/java/Mysweetsystem2024/LoginManager.java
+++ src/main/java/Mysweetsystem2024/LoginManager.java
@@ -1,5 +1,6 @@
 package Mysweetsystem2024;
-
+import java.util.logging.Logger;
+import java.util.logging.Level;
 import java.util.Map;
 import java.util.Set;
 import java.util.HashSet;
@@ -8,7 +9,7 @@ public class LoginManager {
     private final Map<String, User> users;
     private final Set<String> loggedInUsers; // To track logged-in users
 
-    
+     private static final Logger logger = Logger.getLogger(LoginManager.class.getName());
     private String currentUser;
 
     
@@ -21,28 +22,24 @@ public class LoginManager {
         this.loggedInUsers = new HashSet<>();
     }
 
-    public boolean login(String username, String password) {
+public boolean login(String username, String password) {
         if (validateLogin(username, password)) {
             this.currentUser = username; // Set the currentUser to the logged-in username
-            System.out.println("Login successful. Welcome, " + currentUser + "!");
+            logger.log(Level.INFO, "Login successful. Welcome, {0}!", currentUser);
             return true;
         } else {
-            System.out.println("Login failed. Invalid username or password.");
+            logger.log(Level.WARNING, "Login failed. Invalid username or password.");
             return false;
         }
     }
 
+    
     private boolean validateLogin(String username, String password) {
         User user = users.get(username);
         return user != null && user.getPassword().equals(password);
     }
 
-    //public String getCurrentUser() {
-      //  return currentUser;
-    //}
-
-    
-    
+   
 
     public boolean logout(String username) {
         if (username == null) {
